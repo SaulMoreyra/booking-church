a59512ca00aaf9eccf62d6b1e360de54436941f5
SaulMoreyra <armhacam21@gmail.com>
2023-01-30 12:00:05 -0600

    feat: implement libraries

---
+++ src/core/components/index.ts
@@ -0,0 +1,4 @@
+export { default as BannerBooking } from "./BannerBooking";
+export { default as FormBooking } from "./FormBooking";
+export { default as ModalExtraPerson } from "./ModalExtraPerson";
+export { default as CardPerson } from "./CardPerson";
